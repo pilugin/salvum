f6cf6e9133db512a5d7096970d91892e0dce2d8a
Konstantyn Pilugin <pilugin.87@gmail.com>
2014-08-28 16:32:06 +0300

    operators << & >> added for DBus types

--- src/if/dbus/declareDBusMetatypes.cpp
+++ src/if/dbus/declareDBusMetatypes.cpp
@@ -1,15 +1,70 @@
 #include "declareDBusMetatypes.h"
+#include <QtDBus>
 
 class DBusMetatypesRegistrer
 {
 public:
     DBusMetatypesRegistrer()
     {
-	qRegisterMetaType<QVector<int>>();
-	qRegisterMetaType<Pixmap>();
-	qRegisterMetaType<DecodedClusters>();
-	
+        qDBusRegisterMetaType<QVector<int>>();
+        qDBusRegisterMetaType<Pixmap>();
+        qDBusRegisterMetaType<DecodedClusters>();
     }
 };
 
 static DBusMetatypesRegistrer theDBusMetatypesRegistrer;
+
+///////////////// << & >>
+
+const QDBusArgument &operator>>(const QDBusArgument &stream, DecodedClusters &decodedClusters)
+{
+    stream.beginArray();
+    while (!stream.atEnd()) {
+        stream.beginStructure();
+        DecodedClusters::value_type v;
+        stream >> std::get<0>(v) >> std::get<1>(v) >> std::get<2>(v);        
+	stream.endStructure();
+	decodedClusters.push_back(v);
+    }
+    stream.endArray();
+    return stream;
+}
+
+const QDBusArgument &operator>>(const QDBusArgument &stream, Pixmap &pixmap)
+{
+    stream.beginStructure();
+    stream >> std::get<0>(pixmap) >> std::get<1>(pixmap);;
+    stream.beginArray();
+    while (!stream.atEnd()) {
+	int pixel;
+        stream >> pixel;
+        std::get<2>(pixmap).push_back(pixel);
+    }
+    stream.endArray();
+    stream.endStructure();
+    return stream;
+}
+
+QDBusArgument &operator<<(QDBusArgument &stream, const DecodedClusters &decodedClusters)
+{
+    stream.beginArray(decodedClusters.size());
+    for (int i=0; i<decodedClusters.size(); ++i) {
+        stream.beginStructure();
+        stream << std::get<0>(decodedClusters[i]) << std::get<1>(decodedClusters[i]) << std::get<2>(decodedClusters[i]);        
+	stream.endStructure();
+    }
+    stream.endArray();
+    return stream;
+}
+
+QDBusArgument &operator<<(QDBusArgument &stream, const Pixmap &pixmap)
+{
+    stream.beginStructure();
+    stream << std::get<0>(pixmap) << std::get<1>(pixmap);
+    stream.beginArray( std::get<2>(pixmap).size() );
+    for (int i=0; i<std::get<2>(pixmap).size(); ++i) 
+	stream << std::get<2>(pixmap)[i];
+    stream.endArray();
+    stream.endStructure();
+    return stream;
+}
--- src/if/dbus/declareDBusMetatypes.h
+++ src/if/dbus/declareDBusMetatypes.h
@@ -8,4 +8,13 @@ Q_DECLARE_METATYPE(QVector<int>)
 Q_DECLARE_METATYPE(DecodedClusters)
 Q_DECLARE_METATYPE(Pixmap)
 
+#include <QDBusArgument>
+
+const QDBusArgument &operator>>(const QDBusArgument &stream, DecodedClusters &decodedClusters);
+const QDBusArgument &operator>>(const QDBusArgument &stream, Pixmap &pixmap);
+QDBusArgument &operator<<(QDBusArgument &stream, const DecodedClusters &decodedClusters);
+QDBusArgument &operator<<(QDBusArgument &stream, const Pixmap &pixmap);
+
+
+
 #endif
