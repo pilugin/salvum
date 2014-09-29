3b745da1538d3a8a349b1a26c64fdc70372472ea
Konstantyn Pilugin <pilugin.87@gmail.com>
2014-09-29 03:33:44 +0300

    Fix wspaceModel: absolutePath for wspace

--- apps/main-dbus/models/workspacemodel.cpp
+++ apps/main-dbus/models/workspacemodel.cpp
@@ -190,7 +190,7 @@ void WorkspaceModel::loadWspace(const QString &path)
     beginResetModel();
     m_d->items.clear();
     QDir wspaceDir(path);
-    m_d->wspacePath = path;
+    m_d->wspacePath = wspaceDir.absolutePath();
     
     do {
         if (!wspaceDir.exists()) {
